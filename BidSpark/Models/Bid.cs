19c12b4ac9bb1b2708fec65adbbcdd81d717c7b2
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-13 15:13:34 +0545

    Initial Models and Database

---
+++ BidSpark/Models/Bid.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BidSpark.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public double price { get; set; }
+
+        [Required] // is the user is deleted list will be deleted
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+
+        public IdentityUser? IdentityUser { get; set; }
+
+        public int? ListingId { get; set; }
+        [ForeignKey ("ListingId")]
+        public Listing? Listing { get; set; }
+
+    }
+}
---
+++ BidSpark/Models/Listing.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BidSpark.Models
+{
+    public class Listing
+    {
+    
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required double  Price { get; set; }
+        public required string ImagePath { get; set; }
+        public required bool IsSold { get; set; }
+
+        [Required]
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+
+        //Navigation properties
+        public IdentityUser? IdentityUser { get; set; }
+
+        public List<Bid>? Bids { get; set; }
+        public List<Comment>? Comments { get; set; }
+    }
+}
